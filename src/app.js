59b494d4b8550236b1a40652a344232cf599f27b
mlj10 <91034313+mlj10@users.noreply.github.com>
2021-11-18 23:56:01 +0000

    Q 4

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+//creating app
+const app = express();
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Cart app listening at http://localhost:${port}`);
+});
