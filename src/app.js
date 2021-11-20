f41f825ea358f2bfcfe87e1ad1a5295f357bc809
mlj10 <91034313+mlj10@users.noreply.github.com>
2021-11-20 01:01:36 +0000

    done the first ejs template

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 //creating app
 const app = express();
+//handling static HTML and EJS templates
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+  res.render("index");
 });
 
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
